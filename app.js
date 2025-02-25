b348b17654725b6e7d18977f03fb04b8d387eb2c
ajaykumarraghav1711 <ajayraghav399@gmail.com>
2025-02-25 21:53:23 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -46,4 +46,9 @@ app.use("/users",userRouter);
 app.use("/cart",cartRouter);
 app.use("/payment",paymentRouter);
 app.use("/order",orderRouter);
-app.listen(process.env.PORT || 3000);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+
